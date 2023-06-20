d9c9f69e188e18f388335cd9396b88ddc11367b4
Alon <alon.glater@protonmail.com>
2023-06-20 14:59:57 +0300

    Turn Header Buttons into Links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import {
+  CssBaseline,
   AppBar,
   Container,
   Toolbar,
@@ -7,37 +8,54 @@ import {
   Box,
   Button,
 } from "@mui/material";
+import Link from "next/link";
 
-const pages = ["Home", "Blog"];
+const pages = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
 
 export const Header: React.FC = () => {
   return (
-    <AppBar>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              fontWeight: 700,
-              letterSpacing: "0.3rem",
-              textDecoration: "none",
-            }}
-          >
-            esh.
-          </Typography>
-          <Box sx={{ flexGrow: 1 }}>
-            {pages.map((page) => (
-              <Button key={page} sx={{ my: 2, color: "rgba(0, 0, 0, 0.87)" }}>
-                {page}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                fontWeight: 700,
+                letterSpacing: "0.2rem",
+                textDecoration: "none",
+              }}
+            >
+              esh.
+            </Typography>
+            <Box sx={{ flexGrow: 1 }}>
+              {pages.map((page) => (
+                <Button
+                  key={page.title}
+                  sx={{ my: 2, color: "rgba(0, 0, 0, 0.87)" }}
+                >
+                  <Link href={page.path}>{page.title}</Link>
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Toolbar />
+    </>
   );
 };
